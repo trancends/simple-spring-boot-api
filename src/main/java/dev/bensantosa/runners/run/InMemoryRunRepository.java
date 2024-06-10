9fbc9784dedcebfdb5bf531acb538c8c4aed59bb
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-06-10 14:38:45 +0700

    add testsuite for inMemoryRunRepository

---
+++ src/main/java/dev/bensantosa/runners/run/InMemoryRunRepository.java
@@ -0,0 +1,51 @@
+package dev.bensantosa.runners.run;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class InMemoryRunRepository {
+
+  private  final  static Logger log = LoggerFactory.getLogger(InMemoryRunRepository.class);
+  private final List<Run> runs = new ArrayList<>();
+
+  List<Run> findAll() {
+    return runs;
+  }
+
+  Optional<Run> findById(Integer id) {
+    return Optional.ofNullable(runs.stream()
+            .filter(run -> run.id() == id)
+            .findFirst()
+            .orElseThrow(RunNotFoundException::new));  }
+
+  void create(Run run) {
+    runs.add(run);
+  }
+
+  void update(Run run, Integer id) {
+    Optional<Run> existingRun = findById(id);
+    if(existingRun.isPresent()) {
+      var r = existingRun.get();
+      log.info("Updating Existing Run: " + existingRun.get());
+      runs.set(runs.indexOf(r),run);
+    }  }
+
+  void delete(Integer id) {
+    runs.removeIf(run -> run.id().equals(id));
+  }
+
+  private void init() {
+    runs.add(
+        new Run(1, "Run 1 ", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 6, Location.INDOOR,
+            null));
+    runs.add(
+        new Run(2, "Run 2", LocalDateTime.now(), LocalDateTime.now().plus(2, ChronoUnit.HOURS), 10, Location.OUTDOOR,
+            null));
+  }
+}
---
+++ src/test/java/dev/bensantosa/runners/run/InMemoryRunRepositoryTest.java
@@ -0,0 +1,84 @@
+package dev.bensantosa.runners.run;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * InMemoryRunRepositoryTest
+ */
+public class InMemoryRunRepositoryTest {
+
+  InMemoryRunRepository repository;
+
+  @BeforeEach
+  void setUp() {
+    repository = new InMemoryRunRepository();
+    repository.create(new Run(1, "Monday Morning Run", LocalDateTime.now(),
+            LocalDateTime.now().plusMinutes(30), 6, Location.OUTDOOR, null));
+    repository.create(new Run(2, "Wednesday Evening Run", LocalDateTime.now(),
+            LocalDateTime.now().plusMinutes(30), 6, Location.OUTDOOR, null));
+  }
+
+  @Test
+  void shouldFindAllRuns() {
+    List<Run> runs = repository.findAll();
+    assertEquals(2, runs.size(), "Should have return 2 runs");
+  }
+  @Test
+  void shouldFindRunWithValidId() {
+    var run = repository.findById(1).get();
+    assertEquals("Monday Morning Run", run.title());
+    assertNotEquals(3, run.miles());
+  }
+
+  @Test
+  void shouldNotFindRunWithInvalidId() {
+    RunNotFoundException notFoundException = assertThrows(
+            RunNotFoundException.class,
+            () -> repository.findById(3).get()
+    );
+
+    assertEquals("Run Not Found", notFoundException.getMessage());
+  }
+
+  @Test
+  void shouldCreateNewRun() {
+    repository.create(new Run(3,
+            "Friday Morning Run",
+            LocalDateTime.now(),
+            LocalDateTime.now().plusMinutes(30),
+            3,
+            Location.INDOOR,
+            null
+            ));
+    List<Run> runs = repository.findAll();
+    assertEquals(3, runs.size());
+  }
+
+  @Test
+  void shouldUpdateRun() {
+    repository.update(new Run(1,
+            "Monday Morning Run",
+            LocalDateTime.now(),
+            LocalDateTime.now().plusMinutes(30),
+            5,
+            Location.OUTDOOR,null), 1);
+    var run = repository.findById(1).get();
+    assertEquals("Monday Morning Run", run.title());
+    assertEquals(5, run.miles());
+    assertEquals(Location.OUTDOOR, run.location());
+  }
+
+  @Test
+  void shouldDeleteRun() {
+    repository.delete(1);
+    List<Run> runs = repository.findAll();
+    assertEquals(1, runs.size());
+  }
+}
